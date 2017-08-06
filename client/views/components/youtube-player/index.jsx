cbef9d83fc28fc2998c64d912d2a40466f0ae8e5
Tamer Wahba <tamwahba@hotmail.com>
2017-08-05 21:46:19 -0400

    play videos inline for iOS

--- client/views/components/youtube-player/index.jsx
+++ client/views/components/youtube-player/index.jsx
@@ -62,7 +62,7 @@ export class YoutubePlayer extends React.Component {
         id="youtube-player"
         title="Youtube Player"
         className="youtube-player"
-        src="https://www.youtube.com/embed/?enablejsapi=1&controls=0&autoplay=0"
+        src="https://www.youtube.com/embed/?enablejsapi=1&controls=0&autoplay=0?playsinline=1"
       />
     );
   }
