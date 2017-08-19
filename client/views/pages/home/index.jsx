3268d3595520071ded690e75e45a681f97c15830
Tamer Wahba <tamwahba@hotmail.com>
2017-08-19 18:44:49 -0400

    feat(home): add simple home page

--- client/views/pages/home/index.jsx
+++ client/views/pages/home/index.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default () => (
-  <h1>Home</h1>
-);
+import Icon from '../../components/icon';
+
+import logo from '../../icons/logo-text.svg';
+
+import screenshot from './croud-dj-screenshot.png';
+
+import './styles.less';
+
+function HomePage() {
+  return (
+    <div className="home">
+      <Icon alt="croud.dj logo" className="home__logo" data={logo} />
+      <div className="home__hero-container">
+        <img alt="screenshot of croud.dj room page" src={screenshot} />
+      </div>
+      <section className="home__panel home__panel--top">
+        <Link to="/host" className="home__link">Host a new room</Link>
+        <span>to allow your friends to queue songs, and vote on which gets played next.</span>
+      </section>
+      <span className="home__divider">or</span>
+      <section className="home__panel home__panel--bottom">
+        <Link to="/guest" className="home__link">Join an existing one</Link>
+        <span>as a guest to add, and vote on what&#8217;s playing.</span>
+      </section>
+    </div>
+  );
+}
+
+export default HomePage;
